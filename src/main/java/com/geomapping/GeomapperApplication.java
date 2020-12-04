223a89537daee68dbe801c2ab5019a8730c65258
shanib <skunhimon@qatarairways.com.qa>
2020-12-04 16:02:22 +0300

    updates - 2

---
+++ src/main/java/com/geomapping/GeomapperApplication.java
@@ -0,0 +1,13 @@
+package com.geomapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeomapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeomapperApplication.class, args);
+	}
+
+}
