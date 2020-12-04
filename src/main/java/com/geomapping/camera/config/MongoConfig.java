d30bd2ad6f14e8e4ea0fb4eef0e51f757b4a2840
shanib <skunhimon@qatarairways.com.qa>
2020-12-04 18:26:37 +0300

    dfsdfdsf

--- src/main/java/com/geomapping/camera/config/MongoConfig.java
+++ src/main/java/com/geomapping/camera/config/MongoConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
-import com.mongodb.MongoClient;
 
 @Configuration
 public class MongoConfig {
